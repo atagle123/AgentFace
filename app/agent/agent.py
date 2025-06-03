b1c59d7541bf4929939e3ec1ee1295836861ef28
Augusto <augustotaglemontes@gmail.com>
2025-06-02 21:53:21 -0400

    agent module that contains agent class

---
+++ app/agent/agent.py
@@ -0,0 +1,62 @@
+from smolagents import InferenceClientModel, TransformersModel, CodeAgent
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+
+class Agent:
+    def __init__(self, model_id: str = "Qwen/Qwen2.5-Coder-32B-Instruct"):
+        """
+        Initializes the Agent with a model.
+
+        Args:
+            model_id (str): The model identifier to use.
+        """
+        # self.model = TransformersModel(
+        #     model_id=model_id,
+        #      device="cuda",
+        #      max_new_tokens=5000,
+        #  )
+        self.model = InferenceClientModel(
+            model_id=model_id,
+            token=str(HF_TOKEN),
+        )
+        self.tools = []
+
+    def add_tools(self, tools: list) -> None:
+        """
+        Adds tools to the agent.
+
+        Args:
+            tools (list): A list of smolagents tools to add.
+        """
+        self.tools += tools
+
+    def setup_agent(self) -> None:
+        """
+        Sets up the CodeAgent with the current tools and model.
+        """
+        self.agent = CodeAgent(tools=self.tools, model=self.model)
+
+    def __call__(self, prompt: str):
+        """
+        Runs the agent on the given prompt.
+
+        Args:
+            prompt (str): The prompt or task to run.
+
+        """
+        self.agent.run(task=prompt)
+
+
+def main():
+    agent = Agent()
+    agent.setup_agent()
+    prompt = "hola que tal"
+    agent(prompt)
+
+
+if __name__ == "__main__":
+    main()
