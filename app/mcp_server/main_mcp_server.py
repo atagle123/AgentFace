f2845faa910f0b1c1291b8afac728f6d6c18b21e
Augusto <augustotaglemontes@gmail.com>
2025-06-02 21:53:51 -0400

    added main mcp server which builds from subservers

---
+++ app/mcp_server/main_mcp_server.py
@@ -0,0 +1,21 @@
+from fastmcp import FastMCP
+from sub_servers.math_server import math_mcp
+import asyncio
+
+
+main_mcp = FastMCP(
+    name="MainServer",
+    host="127.0.0.1",
+    port="8000",
+    on_duplicate_tools="error",  # error on duplicate tools
+)
+
+
+# Import subserver
+async def setup():
+    await main_mcp.import_server("sub_server", math_mcp)
+
+
+if __name__ == "__main__":
+    asyncio.run(setup())
+    main_mcp.run(transport="streamable-http", host="127.0.0.1", port=8000, path="/mcp")
