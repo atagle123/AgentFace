3c1daa0245dbf491f001dc2f1e7cb1484815ad75
augusto <augustotaglemontes@gmail.com>
2025-06-10 18:06:03 -0400

    Added main modal app run with package structure

---
+++ src/app.py
@@ -0,0 +1,8 @@
+import modal
+
+#from src.backend.manim_agent import manim_app
+#from src.backend.summary_agent import pdf_app
+#from src.frontend.modal_gradio_app import ui_app
+
+
+app = modal.App("full-app") # .include(pdf_app).include(ui_app) # include backend first
